2795c9b1546a025053f7798e6e47a3118c4c57ee
Bruno Russo <bruno.russo@centauro.com.br>
2019-10-10 14:49:58 -0300

    Features done

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-console.log('bananaß')
+const form = document.querySelector('#form');
+
+form.addEventListener('submit', sumbmitEmail);
+
+function sumbmitEmail(e) {
+    e.preventDefault();
+    window.location.href = 'https://www.freecodecamp.com/email-submit';
+}
